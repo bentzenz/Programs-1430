06c4059ddc31943ec9f79282aff5cd7be36594e7
unknown <bentzenz@WKS008916.ion.uwplatt.edu>
2015-10-28 12:35:09 -0500

    new program

---
+++ Program4/Program4.cpp
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------
+//
+// Name:    
+//
+// Course:  CS 1430, Section 
+//
+// Purpose: 
+//
+// Input:   
+//
+// Output:  
+//---------------------------------------------------------------------
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+   return 0;
+}
