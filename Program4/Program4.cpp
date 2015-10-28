2a102925698ee626e8a8bddc8f74a48b02819c2e
unknown <bentzenz@WKS008916.ion.uwplatt.edu>
2015-10-28 12:42:28 -0500

    everything

--- Program4/Program4.cpp
+++ Program4/Program4.cpp
@@ -1,12 +1,12 @@
 //---------------------------------------------------------------------
 //
-// Name:    
+// Name:  ,Zachary Bentzen   
 //
-// Course:  CS 1430, Section 
+// Course:  CS 1430, Section 5  
 //
-// Purpose: 
+// Purpose:  program to help a post office in a small city in Wisconsin print a report of the parcels/boxes that were processed in a week.
 //
-// Input:   
+// Input:    mass, length, width or depth
 //
 // Output:  
 //---------------------------------------------------------------------
@@ -18,5 +18,45 @@ using namespace std;
 
 int main()
 {
+   int mass;
+   cin >> mass;
    return 0;
 }
+//Reads the parcel post table, reading the size of the table first
+//and that many masses and costs.  Assume that the massess are
+//ordered in ascending order by mass in (integer) grams.
+//params: TODO
+void ReadParcelPostTable(int masses[], float costs[], int & sizeOfTable);
+
+ 
+
+//Prints the parcel post table in tabular form. Prints the heading 
+//that includes printing the size of the table. (See sample output)
+//params: TODO
+void PrintParcelPostTable(const int masses[], const float costs[], int sizeOfTable);
+
+ 
+
+//Finds and returns an index of the look-up-mass in the Masses 
+//array. Since the Masses array is ordered in ascending order by 
+//mass, this function returns the index of the first mass 
+//in the masses array such that lookUpMass is less than or equal 
+//to masses[i].
+//On the off chance that no such index can be found, returns -1.
+//params: TODO
+int FindIndexOfMass(const int masses[], int lookUpMass, int size);
+
+ 
+
+//Returns the largest value in the dim array; size is how many items
+//are in the dim array.
+//params: TODO
+float FindLargestDimension( const int dim[], int size);
+
+ 
+
+//Returns the girth of a parcel where girth is calculated as twice 
+//the difference of the sum of the elements in the dim array and the 
+//largest of the dimensions. 
+//params: TODO
+float Girth( const int dim[], int size);
